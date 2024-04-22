300283a56e29c75c0bb51c8606303f811a78442a
Thomas Roux <th.rx@hotmail.fr>
2024-04-22 11:27:22 +0200

    feat(format): Lint initial skeleton code

---
+++ src/opinion/Members.java
@@ -0,0 +1,2 @@
+package opinion;public class Members {
+}
--- src/opinion/SocialNetwork.java
+++ src/opinion/SocialNetwork.java
@@ -11,80 +11,83 @@ import exceptions.NotMemberException;
 
 /**
  * Skeleton for the SocialNetwork
- * 
  */
 public class SocialNetwork implements ISocialNetwork {
+    // Class attribute
+    private int nbMembers;
+    private LinkedList<Members> members;
 
-	@Override
-	public int nbMembers() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    // Constructor
+    public SocialNetwork() {
+        members = new LinkedList<>();
+        nbMembers = 0;
+    }
 
-	@Override
-	public int nbFilms() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    //Instance method
+    @Override
+    public int nbMembers() {
+        return nbMembers;
+    }
 
-	@Override
-	public int nbBooks() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public int nbFilms() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
-	@Override
-	public void addMember(String login, String password, String profile)
-			throws BadEntryException, MemberAlreadyExistsException {
-		// TODO Auto-generated method stub
+    @Override
+    public int nbBooks() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
-	}
 
-	@Override
-	public void addItemFilm(String login, String password, String title,
-			String kind, String director, String scriptwriter, int duration)
-			throws BadEntryException, NotMemberException,
-			ItemFilmAlreadyExistsException {
-		// TODO Auto-generated method stub
+    @Override
+    public void addItemFilm(String login, String password, String title,
+                            String kind, String director, String scriptwriter, int duration)
+            throws BadEntryException, NotMemberException,
+            ItemFilmAlreadyExistsException {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
-	@Override
-	public void addItemBook(String login, String password, String title,
-			String kind, String author, int nbPages) throws BadEntryException,
-			NotMemberException, ItemBookAlreadyExistsException {
-		// TODO Auto-generated method stub
+    @Override
+    public void addItemBook(String login, String password, String title,
+                            String kind, String author, int nbPages) throws BadEntryException,
+            NotMemberException, ItemBookAlreadyExistsException {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
-	@Override
-	public float reviewItemFilm(String login, String password, String title,
-			float mark, String comment) throws BadEntryException,
-			NotMemberException, NotItemException {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public float reviewItemFilm(String login, String password, String title,
+                                float mark, String comment) throws BadEntryException,
+            NotMemberException, NotItemException {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
-	@Override
-	public float reviewItemBook(String login, String password, String title,
-			float mark, String comment) throws BadEntryException,
-			NotMemberException, NotItemException {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public float reviewItemBook(String login, String password, String title,
+                                float mark, String comment) throws BadEntryException,
+            NotMemberException, NotItemException {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
-	@Override
-	public LinkedList<String> consultItems(String title)
-			throws BadEntryException {
-		return new LinkedList<String>();
-	}
+    @Override
+    public LinkedList<String> consultItems(String title)
+            throws BadEntryException {
+        return new LinkedList<String>();
+    }
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
 
 }
