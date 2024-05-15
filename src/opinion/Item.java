8d1f24908c2cfad19efd18f62f5e4b6440d3de40
Thomas Roux <th.rx@hotmail.fr>
2024-05-15 23:14:07 +0200

    feat(ite1.3): Add review support for items
    	- Compute mean of marks
    	- Specific toString method to get the review
    	- Upated general toString method
    
    + prevent user to set null argument on method

--- src/opinion/Item.java
+++ src/opinion/Item.java
@@ -2,11 +2,14 @@ package opinion;
 
 import exceptions.BadEntryException;
 
+import java.util.LinkedList;
+
 public class Item {
     //Class attributes
     protected String title;
     protected String kind;
     protected String addedBy;
+    protected LinkedList<Review> reviews;
 
     /**
      * Constructor for the Item class
@@ -24,12 +27,13 @@ public class Item {
         if (kind == null) {
             throw new BadEntryException("La catégorie du média n'a pas été renseignée");
         }
-        if (addedBy == null){
+        if (addedBy == null) {
             throw new BadEntryException("L'auteur du post n'a pas été renseigné");
         }
         this.title = title;
         this.kind = kind;
         this.addedBy = addedBy;
+        this.reviews = new LinkedList<>();
     }
 
     /**
@@ -48,14 +52,78 @@ public class Item {
     }
 
     /**
+     * Method to add reviews to the Item, if the user used to post the review already sent a review, the old one will be replaced with the new one
+     *
+     * @param content
+     * @param mark
+     * @param addedBy
+     * @throws BadEntryException
+     */
+    public void addReview(String content, float mark, String addedBy) throws BadEntryException {
+        int index = 0;
+        boolean found = false;
+        for (Review r : reviews) {
+            if (r.addedBy.equals(addedBy)) {
+                found = true;
+                reviews.set(index, new Review(content, addedBy, mark));
+            }
+            index++;
+        }
+        if (!found) {
+            reviews.add(new Review(content, addedBy, mark));
+        }
+    }
+
+    /**
+     * Method to compute the mean of all marks leaved in review of the Item.
+     *
+     * @return float
+     */
+    public float getMeanMark() {
+        float result = 0;
+        for (Review r : reviews) {
+            result += r.mark;
+        }
+        return result / reviews.size();
+    }
+
+    /**
+     * Method to generate a string containing all the review of the Item.
+     *
+     * @return String
+     */
+    public String getReviews() {
+        StringBuilder concatenedString = new StringBuilder();
+        concatenedString.append("\n------------------------------ Avis ------------------------\n");
+        if (!reviews.isEmpty()) {
+            for (Review r : reviews) {
+                concatenedString.append(r);
+                concatenedString.append("\n");
+            }
+        } else {
+            concatenedString.append("Pas d'avis");
+        }
+        return concatenedString.toString();
+    }
+
+    /**
      * Return a string containing all the attributes for the Item instance
      *
      * @return String
      */
     @Override
     public String toString() {
-        String temp;
-        temp = "Titre: " + this.title + ", Catégorie: " + this.kind + "Ajouté par: " + this.addedBy;
-        return temp;
+        StringBuilder concatenedString = new StringBuilder();
+        concatenedString.append("Titre: ");
+        concatenedString.append(this.title);
+        concatenedString.append(", Catégorie: ");
+        concatenedString.append(this.kind);
+        if (!Float.isNaN(getMeanMark())) {
+            concatenedString.append(", Note (Moyenne): ");
+            concatenedString.append(getMeanMark());
+        }
+        concatenedString.append(", Ajouté par: ");
+        concatenedString.append(this.addedBy);
+        return concatenedString.toString();
     }
 }
